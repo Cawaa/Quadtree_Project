36a044be74f607048d3f933e98dfae386c0ae503
E244551R <max.poedras@etu.univ-nantes.fr>
2024-12-10 17:18:45 +0100

    Fonction get.go

--- quadtree/get.go
+++ quadtree/get.go
@@ -4,5 +4,37 @@ package quadtree
 // un terrain dont la case le plus en haut à gauche a pour coordonnées
 // (topLeftX, topLeftY)) à partir du qadtree q.
 func (q Quadtree) GetContent(topLeftX, topLeftY int, contentHolder [][]int) {
-	// TODO
+	numTileY := len(contentHolder)
+	numTileX := len(contentHolder[0])
+
+	for y:=0; y<numTileY; y++ {
+		for x:=0; x<numTileX;x++ {
+			absX := topLeftX + x
+			absY := topLeftY + y 
+
+			if absX < 0 || absY < 0 || absX >= q.width || absY >= q.height {
+				contentHolder[y][x] = -1
+			} else {
+				contentHolder[x][y] = q.root.getValueAt(absX,absY)
+			}
+		}
+	}
+}
+
+func (n *node) getValueAt(x, y int) int {
+	if n.isLeaf {
+		return n.content
+	}
+	midX := n.topLeftX + n.width/2
+	midY := n.topLeftY + n.height/2
+
+	if x < midX && y < midY {
+		return n.topLeftNode.getValueAt(x,y)
+	} else if x >= midX && y >= midY {
+		return n.topRightNode.getValueAt(x,y)
+	} else if x < midX && y >= midY {
+		return n.bottomLeftNode.getValueAt(x,y)
+	} else {
+		return n.bottomRightNode.getValueAt(x,y)
+	}
 }
